1ee2a60752ce082fa4d023f068eed8ba4df62927
sarahcsn <chesnesarah@gmail.com>
2017-06-27 11:21:43 +0200

    Changement de titre
    j'ai changé le titre du conteneur MDI

--- Projet Synthèse2/GestionSalaries/ConteneurMDI_mère.Designer.cs
+++ Projet Synthèse2/GestionSalaries/ConteneurMDI_mère.Designer.cs
@@ -28,54 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.gestionDesUtilisateursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.gestionDesSalariésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.menuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gestionDesUtilisateursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDesSalariésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gestionDesUtilisateursToolStripMenuItem,
             this.gestionDesSalariésToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.MdiWindowListItem = this.gestionDesUtilisateursToolStripMenuItem;
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(441, 24);
-			this.menuStrip1.TabIndex = 1;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// gestionDesUtilisateursToolStripMenuItem
-			// 
-			this.gestionDesUtilisateursToolStripMenuItem.Name = "gestionDesUtilisateursToolStripMenuItem";
-			this.gestionDesUtilisateursToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
-			this.gestionDesUtilisateursToolStripMenuItem.Text = "Gestion des Utilisateurs";
-			this.gestionDesUtilisateursToolStripMenuItem.Click += new System.EventHandler(this.gestionDesUtilisateursToolStripMenuItem_Click);
-			// 
-			// gestionDesSalariésToolStripMenuItem
-			// 
-			this.gestionDesSalariésToolStripMenuItem.Name = "gestionDesSalariésToolStripMenuItem";
-			this.gestionDesSalariésToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
-			this.gestionDesSalariésToolStripMenuItem.Text = "Gestion des Salariés";
-			this.gestionDesSalariésToolStripMenuItem.Click += new System.EventHandler(this.gestionDesSalariésToolStripMenuItem_Click);
-			// 
-			// ConteneurMDI_mere
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(441, 261);
-			this.Controls.Add(this.menuStrip1);
-			this.IsMdiContainer = true;
-			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "ConteneurMDI_mere";
-			this.Text = "ConteneurMDI";
-			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-			this.Load += new System.EventHandler(this.Modal_DialConnection);
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MdiWindowListItem = this.gestionDesUtilisateursToolStripMenuItem;
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(441, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gestionDesUtilisateursToolStripMenuItem
+            // 
+            this.gestionDesUtilisateursToolStripMenuItem.Name = "gestionDesUtilisateursToolStripMenuItem";
+            this.gestionDesUtilisateursToolStripMenuItem.Size = new System.Drawing.Size(141, 20);
+            this.gestionDesUtilisateursToolStripMenuItem.Text = "Gestion des Utilisateurs";
+            this.gestionDesUtilisateursToolStripMenuItem.Click += new System.EventHandler(this.gestionDesUtilisateursToolStripMenuItem_Click);
+            // 
+            // gestionDesSalariésToolStripMenuItem
+            // 
+            this.gestionDesSalariésToolStripMenuItem.Name = "gestionDesSalariésToolStripMenuItem";
+            this.gestionDesSalariésToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.gestionDesSalariésToolStripMenuItem.Text = "Gestion des Salariés";
+            this.gestionDesSalariésToolStripMenuItem.Click += new System.EventHandler(this.gestionDesSalariésToolStripMenuItem_Click);
+            // 
+            // ConteneurMDI_mere
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ConteneurMDI_mere";
+            this.Text = "Super programme !";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Modal_DialConnection);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
